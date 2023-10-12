6584ffad9e56350972db20c839b0373267446725
Booker Robinson <robinson.booker11@gmail.com>
2023-10-12 09:53:09 -0400

    make canvas 100% screen size

--- FluidSimMainFolder/Main.js
+++ FluidSimMainFolder/Main.js
@@ -4,7 +4,8 @@
 
 const canvas = document.querySelector("#screen");
 const ctx = canvas.getContext("2d");
-
+ctx.canvas.width  = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
 ctx.beginPath();
 ctx.arc(50, 50, 10, 0, 2 * Math.PI);
 ctx.fill()
