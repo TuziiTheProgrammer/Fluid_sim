cb49046f1bde860290ea8d678a96b6023580d148
Booker Robinson <robinson.booker11@gmail.com>
2023-10-12 09:13:22 -0400

    draw circle on canvas

--- FluidSimMainFolder/Main.js
+++ FluidSimMainFolder/Main.js
@@ -17,3 +17,17 @@ const something =()=>{
 }
 
 something()
+
+
+
+
+
+
+
+
+const canvas = document.querySelector("#screen");
+const ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+ctx.stroke();
