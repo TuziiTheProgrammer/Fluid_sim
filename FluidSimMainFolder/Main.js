9f0a06a4a95bec85d654d81ebadc8f0eece4fc7b
TuzTheProgrammer <razerforest101@gmail.com>
2023-10-12 16:01:42 +0200

    Update 0.0.2

--- FluidSimMainFolder/Main.js
+++ FluidSimMainFolder/Main.js
@@ -1,15 +1,13 @@
 
 
-
-
-const canvas = document.querySelector("#screen");
-
 canvas.addEventListener("click", () => console.log("Clicked!"))
 
+const canvas = document.querySelector("#screen");
 const ctx = canvas.getContext("2d");
 ctx.canvas.width  = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 ctx.beginPath();
-ctx.arc(50, 50, 10, 0, 2 * Math.PI);
+ctx.arc(window.innerWidth/2, window.innerHeight/2, 10, 0, 2 * Math.PI);
 ctx.fill()
 ctx.stroke();
+
